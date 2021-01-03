2107ef1c80e616de511913f41bd8fcf2e8ba341f
edwardmccormick <ted.mccormick@gmail.com>
2021-01-03 01:20:19 -0600

    Big update - character attributes are implemented, now working on skills.

--- js/diceroller.js
+++ js/diceroller.js
@@ -1,5 +1,62 @@
 "use strict"
 
+
+/* Events fired on the drag target */
+
+document.addEventListener("dragstart", function(event) {
+    // The dataTransfer.setData() method sets the data type and the value of the dragged data
+    event.dataTransfer.setData("Text", event.target.id);
+
+
+    // Change the opacity of the draggable element and add a red border
+    event.target.style.opacity = "0.4";
+    event.target.style.border = "3px dotted red";
+});
+
+
+// Finished dragging - reset the opacity and border
+document.addEventListener("dragend", function(event) {
+        event.target.style.opacity = "1";
+    event.target.style.border = "";
+});
+
+/* Events fired on the drop target */
+
+// When the draggable element enters the droptarget, change the DIVS's border style
+document.addEventListener("dragenter", function(event) {
+    if ( event.target.className == "droptarget" ) {
+        event.target.style.border = "3px dotted red";
+    }
+});
+
+// By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+
+// When the draggable p element leaves the droptarget, reset the DIVS's border style
+document.addEventListener("dragleave", function(event) {
+    if ( event.target.className == "droptarget" ) {
+        event.target.style.border = "";
+    }
+});
+
+/* On drop - Prevent the browser default handling of the data (default is open as link on drop)
+   Reset the color of the output text and DIV's border color
+   Get the dragged data with the dataTransfer.getData() method
+   The dragged data is the id of the dragged element
+   Append the dragged element into the drop element
+*/
+document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    if ( event.target.className == "droptarget" ) {
+
+        event.target.style.border = "";
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
+});
+
 function roll(x) {
     return Math.floor(Math.random() * (x))+1;
 }
@@ -42,26 +99,27 @@ function calculateProf() {
     proficiencyBonus.innerHTML = "+ " + profBonus;
 }
 
-function updateCharacter() {
-    character.strength = parseInt(characterStrength.value);
-    character.dexterity = parseInt(characterDexterity.value);
-    character.constitution = parseInt(characterConstitution.value);
-    character.intelligence = parseInt(characterIntelligence.value);
-    character.wisdom = parseInt(characterWisdom.value);
-    character.charisma = parseInt(characterCharisma.value);
-    characterCreation();
-}
+// function updateCharacter() {
+//     character.strength = parseInt(characterStrength.value);
+//     character.dexterity = parseInt(characterDexterity.value);
+//     character.constitution = parseInt(characterConstitution.value);
+//     character.intelligence = parseInt(characterIntelligence.value);
+//     character.wisdom = parseInt(characterWisdom.value);
+//     character.charisma = parseInt(characterCharisma.value);
+//     characterCreation();
+// }
 
-function characterCreation() {
+function pointBuyCreation() {
     var characterCost = 0;
-    for (var i = 0; i < Object.values(character).length; i++) {
-        if (Object.values(character)[i] == 9) {characterCost++}
-        else if (Object.values(character)[i] == 10) {characterCost += 2}
-        else if (Object.values(character)[i] == 11) {characterCost += 3}
-        else if (Object.values(character)[i] == 12) {characterCost += 4}
-        else if (Object.values(character)[i] == 13) {characterCost += 5}
-        else if (Object.values(character)[i] == 14) {characterCost += 7}
-        else if (Object.values(character)[i] == 15) {characterCost += 9}
+    var attributes = [strPointBuy, dexPointBuy, conPointBuy, intPointBuy, wisPointBuy, chaPointBuy]
+    for (var i = 0; i < attributes.length; i++) {
+        if (attributes[i].value == 9) {characterCost++}
+        else if (attributes[i].value == 10) {characterCost += 2}
+        else if (attributes[i].value == 11) {characterCost += 3}
+        else if (attributes[i].value == 12) {characterCost += 4}
+        else if (attributes[i].value == 13) {characterCost += 5}
+        else if (attributes[i].value == 14) {characterCost += 7}
+        else if (attributes[i].value == 15) {characterCost += 9}
             }
     pointTotals.innerHTML = characterCost + "/" + characterCostGoal;
 }
@@ -136,27 +194,125 @@ function diceCards(e) {
     for (var i = 1; i <= attributeRolls.value; i++) {
         var output = rolls(diceToRoll.value, 6)
         // diceOutput.innerHTML += "Array number " + i +" is: " + output + ". Discarding the lowest roll: " + idLowest(output, parseInt(diceToDrop.innerText)) + ". The total for this array is: " + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ ".<br>"
-        var html = '<div className="card draggable border border-dark" class="col-3">'
-            html += '<div className="card-body">'
-            html +=  '<h5 className="card-title">Attribute Roll Number ' + i + '</h5>'
+        var html = '<div class="card border border-primary col-3" id="'+i+'">'
+        html += '<div class="card-body">'
+        html +=  '<h5 class="card-title">Attribute Roll Number ' + i + '</h5>'
 
-        if (diceToRoll.value == 5 ) {html +=  '<p className="card-text">The output of these rolls are: ' + output + '. The lowest rolls are: ' + idLowest(output, parseInt(diceToDrop.innerText)) + '; dropping those from your total. The total for your rolls are: <strong>' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</strong>.</p></div></div>'}
-        if (diceToRoll.value == 4 ) {html +=  '<p className="card-text">The output of these rolls are: ' + output + '. The lowest roll: ' + idLowest(output, parseInt(diceToDrop.innerText)) + '; dropping that from your total. The total for these rolls are: <strong>' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</strong>.</p></div></div>'}
-            if (diceToRoll.value == 3) {html +=  '<p className="card-text">The output of this array is: ' + output + '. You only rolled three dice, so no dice to drop. The total for these rolls is: <strong>' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</strong>.</p></div></div>'}
+        if (diceToRoll.value == 5 ) {html +=  '<p class="card-text">The output of these rolls are: ' + output + '. The lowest rolls are: ' + idLowest(output, parseInt(diceToDrop.innerText)) + '; dropping those from your total. The total for your rolls are: <h4><span class="badge badge-primary text-center" id="total' + i +'" draggable="true">' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</span></h4>.</p></div></div>'}
+        if (diceToRoll.value == 4 ) {html +=  '<p class="card-text">The output of these rolls are: ' + output + '. The lowest roll: ' + idLowest(output, parseInt(diceToDrop.innerText)) + '; dropping that from your total. The total for these rolls are: <h4><span class="badge badge-primary text-center" id="total' + i +'" draggable="true">' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</span></h4>.</p></div></div>'}
+        if (diceToRoll.value == 3) {html +=  '<p className="card-text">The output of this array is: ' + output + '. You only rolled three dice, so no dice to drop. The total for these rolls is: <h4><span class="badge badge-primary text-center" id="total' + i +'" draggable="true">' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</span></h4>.</div></p></div></div>'}
         diceOutput.innerHTML += html
     }
+    strRollInput.innerHTML = 'Str';
+    dexRollInput.innerHTML = 'Dex';
+    conRollInput.innerHTML = 'Con';
+    intRollInput.innerHTML = 'Int';
+    wisRollInput.innerHTML = 'Wis';
+    chaRollInput.innerHTML = 'Cha';
+}
+
+function rollCharacterFinalize(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    character.strength = parseInt(strRollInput.innerText.slice(3));
+    character.dexterity = parseInt(dexRollInput.innerText.slice(3));
+    character.constitution = parseInt(conRollInput.innerText.slice(3));
+    character.intelligence = parseInt(intRollInput.innerText.slice(3));
+    character.wisdom = parseInt(wisRollInput.innerText.slice(3));
+    character.charisma = parseInt(chaRollInput.innerText.slice(3));
+    showCharacterStats()
+}
+
+function typeCharacterFinalize(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    character.strength = parseInt(strTyped.value);
+    character.dexterity = parseInt(dexTyped.value);
+    character.constitution = parseInt(conTyped.value);
+    character.intelligence = parseInt(intTyped.value);
+    character.wisdom = parseInt(wisTyped.value);
+    character.charisma = parseInt(chaTyped.value);
+    showCharacterStats();
+}
+
+function pointBuyCharacterFinalize(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    character.strength = parseInt(strPointBuy.value);
+    character.dexterity = parseInt(dexPointBuy.value);
+    character.constitution = parseInt(conPointBuy.value);
+    character.intelligence = parseInt(intPointBuy.value);
+    character.wisdom = parseInt(wisPointBuy.value);
+    character.charisma = parseInt(chaPointBuy.value);
+    showCharacterStats();
+}
+
+function showCharacterStats() {
+    var skills = [acrobatics, animalhandling, arcana, athletics, deception, historySkill, insight, intimidation, investigation, medicine, nature, perception, performance, persuasion, religion,sleightOfHand, stealth, survival]
+    var skillstext = [acrobaticsText, animalhandlingText, arcanaText, athleticsText, deceptionText, historySkillText, insightText, intimidationText, investigationText, medicineText, natureText, perceptionText, performanceText, persuasionText, religionText, sleightOfHandText, stealthText, survivalText]
+    var skillAttribute = [character.dexterity, character.wisdom, character.intelligence, character.strength, character.charisma, character.intelligence, character.wisdom, character.charisma, character.intelligence, character.wisdom, character.intelligence, character.wisdom, character.charisma, character.charisma, character.intelligence, character.dexterity, character.dexterity, character.wisdom]
+    strDisplay.value = character.strength;
+    dexDisplay.value = character.dexterity;
+    conDisplay.value = character.constitution;
+    intDisplay.value = character.intelligence;
+    wisDisplay.value = character.wisdom;
+    chaDisplay.value = character.charisma;
+    for (var i = 0; i < skills.length; i++) {
+        var count = 0
+        if (attributeModifier(skillAttribute[i]) < 0){
+        if (skills[i].checked === false) {skillstext[i].innerHTML = "<strong>" + attributeModifier(skillAttribute[i]) + "</strong>"};
+        if (skills[i].checked === true) {skillstext[i].innerHTML = "<strong>" + (parseInt(attributeModifier(skillAttribute[i])) + parseInt(profBonus)) + "</strong>";
+        count++
+        }
+        }
+        else {
+            if (skills[i].checked === false) {skillstext[i].innerHTML = "<strong> +" + attributeModifier(skillAttribute[i]) + "</strong>"};
+            if (skills[i].checked === true) {skillstext[i].innerHTML = "<strong> +" + (parseInt(attributeModifier(skillAttribute[i])) + parseInt(profBonus)) + "</strong>";
+            count++}
+        }
+
+    }
+    // document.getElementById('numberOfSkills').innerHTML = "<strong>" + (parseInt(getElementById('numberOfSkills').innerText) - count) + "</strong>"
+}
+
+function attributeModifier (x) {
+    return Math.floor((x-10)/2)
 }
 
 var proficiencyBonus = document.getElementById('profBonus');
 var characterLevel = document.getElementById('pcLevel');
 var characterClass = document.getElementById('pcClass');
-var characterStrength = document.getElementById('str');
-var characterDexterity = document.getElementById('dex');
-var characterConstitution = document.getElementById('con');
-var characterIntelligence = document.getElementById('int');
-var characterWisdom = document.getElementById('wis');
-var characterCharisma = document.getElementById('cha');
+var strDisplay = document.getElementById('strDisplay')
+var dexDisplay = document.getElementById('dexDisplay')
+var conDisplay = document.getElementById('conDisplay')
+var intDisplay = document.getElementById('intDisplay')
+var wisDisplay = document.getElementById('wisDisplay')
+var chaDisplay = document.getElementById('chaDisplay')
+
+var strTyped = document.getElementById('strTyped');
+var dexTyped = document.getElementById('dexTyped');
+var conTyped = document.getElementById('conTyped');
+var intTyped = document.getElementById('intTyped');
+var wisTyped = document.getElementById('wisTyped');
+var chaTyped = document.getElementById('chaTyped');
+
+var strPointBuy = document.getElementById('strpointbuy');
+var dexPointBuy = document.getElementById('dexpointbuy');
+var conPointBuy = document.getElementById('conpointbuy');
+var intPointBuy = document.getElementById('intpointbuy')
+var wisPointBuy = document.getElementById('wispointbuy')
+var chaPointBuy = document.getElementById('chapointbuy')
+
+var strRollInput = document.getElementById('strinput')
+var dexRollInput = document.getElementById('dexinput')
+var conRollInput = document.getElementById('coninput')
+var intRollInput = document.getElementById('intinput')
+var wisRollInput = document.getElementById('wisinput')
+var chaRollInput = document.getElementById('chainput')
+
+var rollFinalize = document.getElementById('finalizeassign')
+var typedFinalize = document.getElementById('finalizetyped')
+var pointBuyFinalize = document.getElementById('finalizepointbuy')
+
 var pointTotals = document.getElementById('creation')
+
 var acrobatics = document.getElementById('acrobatics');
 var animalhandling = document.getElementById('animalhandling');
 var arcana = document.getElementById('arcana');
@@ -175,19 +331,53 @@ var religion = document.getElementById('religion');
 var sleightOfHand = document.getElementById('sleightofhand');
 var stealth = document.getElementById('stealth');
 var survival = document.getElementById('survival');
+
+var acrobaticsText = document.getElementById('acrobaticstext');
+var animalhandlingText = document.getElementById('animalhandlingtext');
+var arcanaText = document.getElementById('arcanatext');
+var athleticsText = document.getElementById('athleticstext');
+var deceptionText = document.getElementById('deceptiontext');
+var historySkillText = document.getElementById('historytext');
+var insightText = document.getElementById('insighttext');
+var intimidationText = document.getElementById('intimidationtext');
+var investigationText = document.getElementById('investigationtext');
+var medicineText = document.getElementById('medicinetext');
+var natureText = document.getElementById('naturetext');
+var perceptionText = document.getElementById('perceptiontext');
+var performanceText = document.getElementById('performancetext');
+var persuasionText = document.getElementById('persuasiontext');
+var religionText = document.getElementById('religiontext');
+var sleightOfHandText = document.getElementById('sleightofhandtext');
+var stealthText = document.getElementById('stealthtext');
+var survivalText = document.getElementById('survivaltext');
+
 var diceToRoll = document.getElementById('dice')
 var diceToDrop = document.getElementById('droppedDice')
 var attributeRolls = document.getElementById('attributeRolls')
 var diceOutput = document.getElementById('diceOutput')
 var rollDice = document.getElementById('rolldice')
 
+var pointBuyTab = document.getElementById('character-pointbuy-tab')
+
+//This governs only the first two selections - character and level
 characterLevel.addEventListener('change', calculateProf);
 characterClass.addEventListener('change', parseSkills)
-characterStrength.addEventListener('change', updateCharacter);
-characterDexterity.addEventListener('change', updateCharacter);
-characterConstitution.addEventListener('change', updateCharacter);
-characterIntelligence.addEventListener('change', updateCharacter);
-characterWisdom.addEventListener('change', updateCharacter);
-characterCharisma.addEventListener('change', updateCharacter);
+
+//This section is for the point buy section
+strPointBuy.addEventListener('change', pointBuyCreation);
+dexPointBuy.addEventListener('change', pointBuyCreation);
+conPointBuy.addEventListener('change', pointBuyCreation);
+intPointBuy.addEventListener('change', pointBuyCreation);
+wisPointBuy.addEventListener('change', pointBuyCreation);
+chaPointBuy.addEventListener('change', pointBuyCreation);
+
+pointBuyTab.addEventListener('click', pointBuyCreation);
+
 diceToRoll.addEventListener('change', onlyRollThree)
 rollDice.addEventListener('click', diceCards)
+rollFinalize.addEventListener('click',rollCharacterFinalize)
+typedFinalize.addEventListener('click', typeCharacterFinalize)
+pointBuyFinalize.addEventListener('click', pointBuyCharacterFinalize)
+
+//Here are all the listeners for the skills
+// acrobatics.addEventListener('change')
--- js/sandbox.js
+++ js/sandbox.js
@@ -1,26 +1,27 @@
+var character = [] // Create the character object so that we can populate it below
+
 /* Events fired on the drag target */
 
 document.addEventListener("dragstart", function(event) {
     // The dataTransfer.setData() method sets the data type and the value of the dragged data
     event.dataTransfer.setData("Text", event.target.id);
 
-    // Output some text when starting to drag the p element
-    document.getElementById("demo").innerHTML = "Started to drag the p element.";
 
     // Change the opacity of the draggable element
     event.target.style.opacity = "0.4";
     event.target.style.border = "3px dotted red";
-    /* event.target.innerHTML = */});
+});
 
 // While dragging the p element, change the color of the output text
-document.addEventListener("drag", function(event) {
-    document.getElementById("demo").style.color = "red";
-});
+// document.addEventListener("drag", function(event) {
+    // document.getElementById("demo").style.color = "red";
+// });
 
 // Output some text when finished dragging the p element and reset the opacity
 document.addEventListener("dragend", function(event) {
-    document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+    // document.getElementById("demo").innerHTML = "Finished dragging the p element.";
     event.target.style.opacity = "1";
+    event.target.style.border = "";
 });
 
 /* Events fired on the drop target */
@@ -53,7 +54,7 @@ document.addEventListener("dragleave", function(event) {
 document.addEventListener("drop", function(event) {
     event.preventDefault();
     if ( event.target.className == "droptarget" ) {
-        document.getElementById("demo").style.color = "";
+        // document.getElementById("demo").style.color = "";
         event.target.style.border = "";
         var data = event.dataTransfer.getData("Text");
         event.target.appendChild(document.getElementById(data));
@@ -100,8 +101,8 @@ function diceCards(e) {
     for (var i = 1; i <= attributeRolls.value; i++) {
         var output = rolls(diceToRoll.value, 6)
         // diceOutput.innerHTML += "Array number " + i +" is: " + output + ". Discarding the lowest roll: " + idLowest(output, parseInt(diceToDrop.innerText)) + ". The total for this array is: " + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ ".<br>"
-        var html = '<div className="card border border-dark" class="col-3" id="'+i+'">'
-        html += '<div className="card-body">'
+        var html = '<div className="card border border-primary" class="col-3" id="'+i+'">'
+        html += '<div className="card-body border border-primary">'
         html +=  '<h5 className="card-title">Attribute Roll Number ' + i + '</h5>'
 
         if (diceToRoll.value == 5 ) {html +=  '<p className="card-text">The output of these rolls are: ' + output + '. The lowest rolls are: ' + idLowest(output, parseInt(diceToDrop.innerText)) + '; dropping those from your total. The total for your rolls are: <h4><span class="badge badge-primary" id="total' + i +'" draggable="true">' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</span></h4>.</p></div></div>'}
@@ -109,6 +110,43 @@ function diceCards(e) {
         if (diceToRoll.value == 3) {html +=  '<p className="card-text">The output of this array is: ' + output + '. You only rolled three dice, so no dice to drop. The total for these rolls is: <h4><span class="badge badge-primary" id="total' + i +'" draggable="true">' + (parseInt(output[0]) + parseInt(output[1]) + parseInt(output [2]) )+ '</span></h4>.</div></p></div></div>'}
         diceOutput.innerHTML += html
     }
+    strRollInput.innerHTML = 'Str';
+    dexRollInput.innerHTML = 'Dex';
+    conRollInput.innerHTML = 'Con';
+    intRollInput.innerHTML = 'Int';
+    wisRollInput.innerHTML = 'Wis';
+    chaRollInput.innerHTML = 'Cha';
+}
+
+function rollCharacterFinalize(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    character.strength = parseInt(strRollInput.innerText.slice(3));
+    character.dexterity = parseInt(dexRollInput.innerText.slice(3));
+    character.constitution = parseInt(conRollInput.innerText.slice(3));
+    character.intelligence = parseInt(intRollInput.innerText.slice(3));
+    character.wisdom = parseInt(wisRollInput.innerText.slice(3));
+    character.charisma = parseInt(chaRollInput.innerText.slice(3));
+    showCharacterStats()
+}
+
+function typeCharacterFinalize(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    character.strength = parseInt(strTyped.value);
+    character.dexterity = parseInt(dexTyped.value);
+    character.constitution = parseInt(conTyped.value);
+    character.intelligence = parseInt(intTyped.value);
+    character.wisdom = parseInt(wisTyped.value);
+    character.charisma = parseInt(chaTyped.value);
+    showCharacterStats();
+}
+
+function showCharacterStats() {
+    strDisplay.value = character.strength;
+    dexDisplay.value = character.dexterity;
+    conDisplay.value = character.constitution;
+    intDisplay.value = character.intelligence;
+    wisDisplay.value = character.wisdom;
+    chaDisplay.value = character.charisma;
 }
 
 var diceToRoll = document.getElementById('dice')
@@ -117,5 +155,36 @@ var attributeRolls = document.getElementById('attributeRolls')
 var diceOutput = document.getElementById('diceOutput')
 var rollDice = document.getElementById('rolldice')
 
+//These are the new variables that I'll need to add back to diceroller.js
+
+var strDisplay = document.getElementById('strDisplay')
+var dexDisplay = document.getElementById('dexDisplay')
+var conDisplay = document.getElementById('conDisplay')
+var intDisplay = document.getElementById('intDisplay')
+var wisDisplay = document.getElementById('wisDisplay')
+var chaDisplay = document.getElementById('chaDisplay')
+
+var strTyped = document.getElementById('strTyped');
+var dexTyped = document.getElementById('dexTyped');
+var conTyped = document.getElementById('conTyped');
+var intTyped = document.getElementById('intTyped');
+var wisTyped = document.getElementById('wisTyped');
+var chaTyped = document.getElementById('chaTyped');
+
+var strRollInput = document.getElementById('strinput')
+var dexRollInput = document.getElementById('dexinput')
+var conRollInput = document.getElementById('coninput')
+var intRollInput = document.getElementById('intinput')
+var wisRollInput = document.getElementById('wisinput')
+var chaRollInput = document.getElementById('chainput')
+
+var rollFinalize = document.getElementById('finalizeassign')
+var typedFinalize = document.getElementById('finalizetyped')
+
 diceToRoll.addEventListener('change', onlyRollThree)
 rollDice.addEventListener('click', diceCards)
+
+//New Listeners
+
+rollFinalize.addEventListener('click',rollCharacterFinalize)
+typedFinalize.addEventListener('click', typeCharacterFinalize)
